da44335e87e774055f9666bbdde03558014f1b72
NimWick638 <wnimantha638@gmail.com>
2025-08-05 13:18:43 +0530

    Define Motor Control Pins

---
+++ RobotWeelBase/sketch_aug5a/sketch_aug5a.ino
@@ -0,0 +1,6 @@
+#define IN1 5  // Motor A direction
+#define IN2 6  // Motor A direction
+#define ENA 3  // Motor A speed (PWM)
+#define IN3 9  // Motor B direction
+#define IN4 10 // Motor B direction
+#define ENB 11 // Motor B speed (PWM)
