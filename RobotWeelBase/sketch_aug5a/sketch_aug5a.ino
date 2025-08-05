aabd68900d09e351a3d9a188e88358a8b44fd3bd
NimWick638 <wnimantha638@gmail.com>
2025-08-05 13:20:57 +0530

    Initialize Serial Communication and Motor Pins in Setup

--- RobotWeelBase/sketch_aug5a/sketch_aug5a.ino
+++ RobotWeelBase/sketch_aug5a/sketch_aug5a.ino
@@ -4,3 +4,21 @@
 #define IN3 9  // Motor B direction
 #define IN4 10 // Motor B direction
 #define ENB 11 // Motor B speed (PWM)
+
+void setup() {
+  Serial.begin(9600); // Start serial communication
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(ENA, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  pinMode(ENB, OUTPUT);
+  // Initialize motors to stopped state
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+  analogWrite(ENA, 0);
+  analogWrite(ENB, 0);
+  Serial.println("Arduino Ready");
+}
